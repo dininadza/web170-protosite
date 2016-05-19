6ad582be5ed81b6e6be31e61719a6ec062f9cbef
Dini Nadzaruddin <dini.nurdayana@gmail.com>
2016-05-18 17:08:29 -0700

    09) Creating a Custom Posts Feed with Featured Images for Your Site
    09) Creating a Custom Posts Feed with Featured Images for Your Site

--- sidebar.php
+++ sidebar.php
@@ -1,29 +1,31 @@
 <!--aside content STARTS-->
 <aside>
-<img src="<?php bloginfo('template_directory');?>/images/team.jpg" alt="company motto" class="companyinfo"/>
-<ul>
 <?php 
-
 if(is_page()){
+	
 if($post->post_parent) { 
+
 echo '<h2>'.get_the_title($post->post_parent). '</h2>'; 
 echo '<ul>';
 wp_list_pages(array('title_li' => '', 'child_of' => $post->post_parent,)); 
 
 echo '<ul>';
+
 } else {  
+echo '<h2>'.get_the_title($post->ID).'</h2>';
+echo '<ul>';
 wp_list_pages(array('title_li' => '','child_of' => $post->ID,));
+
+echo '</ul>';
 }
 
+}
 if(!(is_page())){
-	
 	echo '<h2>Blog</h2>';
 	echo '<ul>';
 wp_list_categories(array('title_li' => '',)); 
 echo '<ul>'; 
 }
 ?>
-</ul>
 </aside>
 <!--aside content ENDS-->
-</section>
--- single.php
+++ single.php
@@ -1,18 +1,18 @@
 <?php get_header(); ?>
+<!--begin text-->
 <section class="aboutUs">
-<article id="content">
+<article id="single">
 <?php 
 //if theres post - while post, get the id of the post
 if(have_posts()) : while(have_posts()) : the_post(); //start loop 1 that pulls the SEO text ?>
-<article id="post-<?php the_ID();?>" class="postings">
-<h2><a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title(); ?>"><?php the_title(); ?></a></h2>
+<h2><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
 <small>Posted on <?php the_time('F j, Y'); ?> by <?php the_author();?> in <?php the_category(', '); ?></small>
-<a href="<?php the_permalink();?>"><?php the_post_thumbnail('thumbnail');?></a>
-<?php the_excerpt();?>
-</article>
+
+<?php the_post_thumbnail('large');?>
+<?php the_content('');?>
 <?php endwhile; endif;?>
-<p>index.php</p>
+<p>single.php</p>
 </article>
 <!--main content ENDS-->
-<?php get_sidebar(); ?>
+<?php get_sidebar();?>
 <?php get_footer();?> 
