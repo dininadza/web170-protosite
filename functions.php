835b4fa7a770a6d2f57aa7b63faecf23c7098adc
Dini Nadzaruddin <dini.nurdayana@gmail.com>
2016-06-03 12:18:37 -0700

    10, 11 & 12 -> creating pull quotes, widgets on sidebar & SEO

--- functions.php
+++ functions.php
@@ -48,4 +48,36 @@ register_sidebar();
 //make thumbnails 
 add_theme_support('post-thumbnails'); 
 //
+
+
+//enable excerpts 
+add_post_type_support( 'page', 'excerpt' ); 
+//
+
+
+//my title tag
+function get_my_title_tag() {
+ global $post; 
+ 
+ if(is_front_page()) { 
+ 
+ 	bloginfo('description'); //get site's tagline 
+ } elseif (is_page() || is_single() ) { //for pages or postings 
+	 the_title(); //the title of page/postings 
+ } else { //for everything else 
+	 bloginfo('description'); //else, site's tagline 
+ }
+	if ($post->post_parent) {
+	
+		echo ' | '; //seperator 
+		echo get_the_title($post->post_parent); //get the title of the parent page 
+	}
+	 
+ 	echo ' | '; //seperator
+ 	bloginfo('name'); //name of site
+	echo ' | ';  //seperator
+ 	echo 'Seattle, WA';  //location of the business 
+}
+
+//end title tag
 ?>
